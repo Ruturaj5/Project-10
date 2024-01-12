1b0a4a4cc523bbecfa40bc5fab4800f1ef1c90e5
Ruturaj5 <156278942+Ruturaj5@users.noreply.github.com>
2024-01-12 14:00:28 +0530

    By Ruturaj
    by Branch

--- Git PushPull 10th Project 12 January/src/com/cjc/Demo10/Git_Demo10_PushPull.java
+++ Git PushPull 10th Project 12 January/src/com/cjc/Demo10/Git_Demo10_PushPull.java
@@ -11,12 +11,18 @@ public class Git_Demo10_PushPull
 	{
 		System.out.println("This is Ruturaj1 Method we will perform firdt basic push and pull");
 	}
+
+	public void ruturaj2() 
+	{
+		System.out.println("This is Ruturaj2 Method Branch");
+	}
 	public static void main(String[] args) 
 	{
 	System.out.println("Main Method 10");
 	Git_Demo10_PushPull  m10 = new Git_Demo10_PushPull();
 	m10.ruturaj();
 	m10.ruturaj1();
+	m10.ruturaj2();
 
 	}
 
