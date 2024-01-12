780f0a9400de6f0ed0a8ee7e73b00198a46cba11
Ruturaj5 <156278942+Ruturaj5@users.noreply.github.com>
2024-01-12 13:58:09 +0530

    BY Ruturaj
    1 Method add normal pull

--- Git PushPull 10th Project 12 January/src/com/cjc/Demo10/Git_Demo10_PushPull.java
+++ Git PushPull 10th Project 12 January/src/com/cjc/Demo10/Git_Demo10_PushPull.java
@@ -6,11 +6,17 @@ public class Git_Demo10_PushPull
 	{
 		System.out.println("This is Ruturaj Method we will perform firdt basic push and pull");
 	}
+
+	public void ruturaj1() 
+	{
+		System.out.println("This is Ruturaj1 Method we will perform firdt basic push and pull");
+	}
 	public static void main(String[] args) 
 	{
 	System.out.println("Main Method 10");
 	Git_Demo10_PushPull  m10 = new Git_Demo10_PushPull();
 	m10.ruturaj();
+	m10.ruturaj1();
 
 	}
 
