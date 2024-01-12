4449016e52c8ab4c4761e0e4010b7ca75b739048
rutur <rutur@192.168.1.71>
2024-01-12 13:54:30 +0530

    By Ruturaj

---
+++ Git PushPull 10th Project 12 January/src/com/cjc/Demo10/Git_Demo10_PushPull.java
@@ -0,0 +1,17 @@
+package com.cjc.Demo10;
+
+public class Git_Demo10_PushPull 
+{
+	public void ruturaj() 
+	{
+		System.out.println("This is Ruturaj Method we will perform firdt basic push and pull");
+	}
+	public static void main(String[] args) 
+	{
+	System.out.println("Main Method 10");
+	Git_Demo10_PushPull  m10 = new Git_Demo10_PushPull();
+	m10.ruturaj();
+
+	}
+
+}
